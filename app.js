91dc2313a6973a474794b59099f66dc6d60b4ac8
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-12 14:19:31 -0600

    Render index.html; note CSS and JS are not working right now.

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static(path.join(__dirname + "/resources")));
+
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + "/index.html"))
+	res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.listen(3000, () => console.log("Mental Mindfulness listening on port 3000."))
