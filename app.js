7080f05edc0c6d408664939ee0b2a4e05d93d8de
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-14 13:14:13 -0600

    Make access to folders from HTML more logical.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname + "/resources")));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + "/index.html"));
