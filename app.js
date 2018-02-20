8a9dea6b59e5a867c0815c7240b5e2c158e32f07
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-20 09:43:28 -0600

    Create tables upon startup.

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const 	express = require('express');
+		path = require('path');
+		fs = require('fs');
+		logger = require('morgan');
+		cookieParser = require('cookie-parser');
+		bodyParser = require('body-parser');
+		pg = require('pg');
+		startupSQL = fs.readFileSync('./sql/first_run.sql').toString();
+
 require('dotenv').load();
-const { Client } = require('pg');
+const app = express();
 
-var index = require('./routes/index');
-var login = require('./routes/login');
-var signup = require('./routes/signup');
-var home = require('./routes/home');
-var journal = require('./routes/journal');
-
-
-var app = express();
-
-// view engine setup
+// Set default view engine
 app.set('views', path.join(__dirname, '/routes/views'));
 app.set('view engine', 'pug');
 
@@ -25,11 +20,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
+	Declaration of routes:
+*/
+
+var index = require('./routes/index');
+var login = require('./routes/login');
+var signup = require('./routes/signup');
+var home = require('./routes/home');
+var journal = require('./routes/journal');
+
 app.use('/', index);
 app.use('/login', login);
 app.use('/signup', signup);
 app.use('/home', home);
 app.use('/journal', journal);
+
+/*
+	Express error-catching set-up:
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -49,13 +58,17 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-// Connect to PG database
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
-
-// client.connect();
+/*
+	Postgres set-up and connection:
+*/
+const initOptions = {};
+const pgp = require('pg-promise')(initOptions);
+const db = pgp(process.env.DATABASE_URL);
+// run on start-up
+db.any(startupSQL)
+    .catch((error) => {
+        // error
+        console.error(error);
+    });
 
 module.exports = app;
