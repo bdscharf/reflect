16d8f84dbbd8664f5b53bea7d7a19a30f4319b5b
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-20 23:59:17 -0600

    Add planning.

--- routes/journal.js
+++ routes/journal.js
@@ -6,6 +6,7 @@ router.get("/", (req, res, next) => {
 	res.sendFile(path.join(__dirname + '/views/journal.html'));
 });
 
+// Implement once account_tools.js and user_data.js are complete
 //router.post("/", (req, res, next) => {
 //	res.redirect('/home');
 //});
