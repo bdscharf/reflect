b721fab6e9b7ff3abfe31496f904c5891ac9661f
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-20 23:59:38 -0600

    Add basic layout of how login validation will occur.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var validateLogin = require('../lib/account_tools.js').validateLogin;
 
 router.get('/',(req, res, next) => {
 	res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 router.post('/', (req, res, next) => {
-	console.log(req.body);
-	// VALIDATE USER INPUT HERE
+	var uname = req.body.username;
+	var pword = req.body.password;
+	validateLogin({	"username": uname,
+					"password": pword}, 
+					(callback) =>
+					{
+						console.log(callback);
+					})
 	res.redirect('/home');
 });
 
