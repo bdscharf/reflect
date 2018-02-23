3828fe47a0b82f3732e8fb776de82365e29325ba
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-23 12:46:18 -0600

    Mark out next moves in home routes.

--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+var jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const dom = new JSDOM(path.join(__dirname + 'views/home.html'));
+
 router.get("/", (req, res, next) => {
 	if (req.session.user)
 	{
 		res.sendFile(path.join(__dirname + '/views/home.html'));
+		//getElementById("welcome-back").innerHTML = "Welcome back, " + req.session.username;
 	}
 	else
 	{
