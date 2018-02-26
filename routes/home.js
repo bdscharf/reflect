5c2088f0788de5a43fc8437b852312cfbcc1cc1e
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-25 18:48:17 -0600

    Succesful login now redirects user to their homepage, and responseData issues fixed. Home.html converted to Pug.

--- routes/home.js
+++ routes/home.js
@@ -6,8 +6,7 @@ var queries = require(path.join('../lib/queries'));
 router.get("/", (req, res, next) => {
 	if (req.session.loggedIn)
 	{
-		res.sendFile(path.join(__dirname + '/views/home.html'));
-		//getElementById("welcome-back").innerHTML = "Welcome back, " + req.session.username;
+		res.render('home');
 	}
 	else
 	{
