c10e735297015dd7ba545115ad0fb63b4e4e8051
Benjamin <benjamin.d.scharf@gmail.com>
2018-02-28 17:07:06 -0600

    Add another condition to session checking to avoid crash when user hasn't accessed any site pages upon start.

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ var path = require('path');
 var queries = require(path.join('../lib/queries'));
 
 router.get("/", (req, res, next) => {
-	if (req.session.loggedIn)
+	if (req.session && req.session.loggedIn)
 	{
 		res.render('home', {user : req.session});
 	}
