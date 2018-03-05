19c4d7715f407386fb232fe745abad79b8ad4448
Benjamin <benjamin.d.scharf@gmail.com>
2018-03-05 01:44:29 -0600

    Pull status and timestamp out after an item is dropped.

--- public/scripts/dragdrop.js
+++ public/scripts/dragdrop.js
@@ -11,4 +11,7 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    var timestamp =document.getElementById(data).getAttribute('value');
+    var newStatus = document.getElementById(data).parentElement.id;
+    var sendString = timestamp + "/" + newStatus;
 }
