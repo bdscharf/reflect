af48f86c1ffb2d73b4e6707a8441365fe7df7d33
Benjamin <benjamin.d.scharf@gmail.com>
2018-03-07 02:15:05 -0600

    Remove logging.

--- public/scripts/dragdrop.js
+++ public/scripts/dragdrop.js
@@ -13,6 +13,7 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
     var timestamp =document.getElementById(data).getAttribute('value');
     var newStatus = document.getElementById(data).parentElement.id;
+    console.log("client new status: " + newStatus);
     // redirect to feed information to the server
 	window.location.replace("/pastgoals?ts=" + timestamp + "&status=" + newStatus);
 }
