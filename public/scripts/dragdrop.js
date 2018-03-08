b8f36f5db79983cabb892196bc624fc57b9fe407
Benjamin Scharf <benjamin.d.scharf@gmail.com>
2018-03-08 16:40:31 -0600

    Fix reward handling on drag-and-drop; for now reward appears once dragged to complete column.

--- public/scripts/dragdrop.js
+++ public/scripts/dragdrop.js
@@ -2,18 +2,17 @@ function allowDrop(ev) {
     ev.preventDefault();
 }
 
-function drag(ev, reward) {
-    console.log(reward, ev)
-    ev.dataTransfer.setData("text", ev.target.id + ";" + reward);
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text").split(';');
-    ev.target.appendChild(document.getElementById(data[0]));
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+
     var timestamp =document.getElementById(data).getAttribute('value');
     var newStatus = document.getElementById(data).parentElement.id;
     // redirect to feed information to the server
 	window.location.replace("/pastgoals?ts=" + timestamp + "&status=" + newStatus);
-    document.getElementById(data[0]).innerHTML="<p>Reward: " + data[1] + "</p>"
 }
